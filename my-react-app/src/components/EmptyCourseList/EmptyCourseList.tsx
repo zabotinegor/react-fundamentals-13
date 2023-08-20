795fd45788eb7ec9024e3ac3a1db9c33b6762db9
Yahor Zabotsin <zabotinegor@mail.ru>
2023-08-20 22:13:01 +0200

    added button "Add course" to the empty list

--- my-react-app/src/components/EmptyCourseList/EmptyCourseList.tsx
+++ my-react-app/src/components/EmptyCourseList/EmptyCourseList.tsx
@@ -1,11 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../../common/Button/Button";
 
 const EmptyCourseList = () => {
+  const navigate = useNavigate();
+
+  const handleAddCourse = () => {
+    navigate("/courses/add", { replace: true });
+  };
+
   return (
     <div className="empty-course-list">
       <h2>Course List is Empty</h2>
       <p>Please use "Add New Course" button to add your first course.</p>
-      <Button text="Add New Course" />
+      <Button text="Add New Course" onClick={handleAddCourse} />
     </div>
   );
 };
